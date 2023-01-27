236ad2a08e492d76b20a72583b92e0fd5b3e439d
glapo992 <108743608+glapo992@users.noreply.github.com>
2023-01-27 16:56:01 +0100

    collegato bootstrap e fatto form

--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,20 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
+'''form per login entrambi utenti'''
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+'''
+Form per Registrazione del dottore
+'''
+class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    phoneNumber = StringField('Phone number', validators=[DataRequired()])  #sotto forma di stringa così non è modificabile
+    submit = SubmitField('register')
+
+#TODO:creare altri form per filtrare risultati qua sotto
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,25 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
     
-    return render_template('index.html')
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        #TODO: validare i dati di iscrizione del medico
+        flash('registration requested for user {}, remember_me={}'.format(
+            form.name.data, form.lastname.data, form.phoneNumber.data))
+        return redirect(url_for('index'))
+    return render_template('registration.html', title='Registration', form=form)    
+
