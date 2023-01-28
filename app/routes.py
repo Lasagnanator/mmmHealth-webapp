a41a61433a64c3790e5960bb71f9f49a77cd4f7e
giuliolapovich <giuliolapovich@yahoo.it>
2023-01-28 17:35:50 +0100

    aggiunte un po di pagine e drawio con schema del sito

--- app/routes.py
+++ app/routes.py
@@ -20,4 +20,21 @@ def registration():
         flash('registration requested for user {}'.format(
             form.name.data))
         return redirect(url_for('index'))
-    return render_template('registration.html', title='Registration', form=form)    
+    return render_template('registration.html', title='Registration', form=form) 
+
+
+
+
+#-----------------error handlers--------------------
+@app.errorhandler(404)
+# Intercetta l'errore page not found e lancia la nostra pagina 404
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+# Intercetta l'errore internal server error lancia la nostra pagina 500
+def internal_server_error(error):
+    return render_template('500.html'), 500
+
+#---------------------------------------------------
