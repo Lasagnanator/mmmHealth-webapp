a96434e66b818fc8d761ff5e4c8b99d69f35cfaa
Lasagnanator <d96a07@gmail.com>
2023-01-30 23:17:01 +0100

    Corretta la stringa per la connessione al db

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'postgresql+psycopg2://mhealth:BIGsmeni42@spectralkraken.tk:5432/mhealth'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
