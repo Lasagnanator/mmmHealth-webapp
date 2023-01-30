c866b1f1f3901e72a0f9cab79a4e5acbaf835aee
glapo992 <108743608+glapo992@users.noreply.github.com>
2023-01-30 11:46:17 +0100

    corretto bug nel merge

--- app/models.py
+++ app/models.py
@@ -2,32 +2,35 @@ from datetime import datetime
 from app import db
 
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(64), index=True, unique=True)
-#     email = db.Column(db.String(120), index=True, unique=True)
-#     password_hash = db.Column(db.String(128))
-#     posts = db.relationship('Post', backref='author', lazy='dynamic')
+class User(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     username = db.Column(db.String(64), index=True, unique=True)
+     email = db.Column(db.String(120), index=True, unique=True)
+     password_hash = db.Column(db.String(128))
+     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-#     def __repr__(self):
-#         return '<User {}>'.format(self.username)
+     def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 
-# class Post(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     body = db.Column(db.String(140))
-#     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class Post(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     body = db.Column(db.String(140))
+     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-#     def __repr__(self):
-#         return '<Post {}>'.format(self.body)
+     def __repr__(self):
+         return '<Post {}>'.format(self.body)
 
+
+# da integrare in my_site
+'''
 class login_patient(db.Model):
     patient_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-     def __repr__(self):
+    def __repr__(self):
         return '<login_patient {}>'.format(self.username) #cosa fa?
 
 class login_doctor(db.Model):
@@ -35,7 +38,7 @@ class login_doctor(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-     def __repr__(self):
+    def __repr__(self):
         return '<login_doctor {}>'.format(self.username) #cosa fa?
 
 class patient(db.Model):
@@ -78,3 +81,4 @@ class patient(db.Model):
     
     def __repr__(self):
         return '<patient {}>'.format(self.patient_id) #cosa fa?
+'''
